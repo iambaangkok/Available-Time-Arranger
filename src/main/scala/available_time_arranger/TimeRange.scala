b9472417a8a3633a081f734552fe2582acaad5d5
Baangkok Vanijyananda <iambaangkok@gmail.com>
2023-03-25 12:33:39 +0700

    added TimeRange.isValidTime, added checks to TimeRange.intersect

--- src/main/scala/available_time_arranger/TimeRange.scala
+++ src/main/scala/available_time_arranger/TimeRange.scala
@@ -1,12 +1,20 @@
 package available_time_arranger
 
 class TimeRange(val start: Int = 0, val end: Int = 2400) {
-  def intersect(other: TimeRange): TimeRange = {
+  def intersect(other: TimeRange): TimeRange = if (
+    start.max(other.start) <= end.min(other.end)
+  ) {
     new TimeRange(start.max(other.start), end.min(other.end))
+  } else {
+    new TimeRange(start, end)
   }
 
   def in(other: TimeRange): Boolean = {
     other.start <= start && end <= other.end
+  }
+
+  def isValidTime: Boolean = {
+    start >= 0 && start <= 2400 && end >= 0 && end <= 2400 && start <= end
   }
 
   override def toString: String = {
