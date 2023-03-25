f5b36e064d638d6ef445ac92e1691c05e889a977
Baangkok Vanijyananda <iambaangkok@gmail.com>
2023-03-25 12:34:48 +0700

    added findAvailableTimeAndPeopleV2 which supports multiple timeRanges in the same day, findTimeRanges (for use in fATAPV2)

--- src/main/scala/available_time_arranger/Main.scala
+++ src/main/scala/available_time_arranger/Main.scala
@@ -8,7 +8,10 @@ import scala.annotation.tailrec
 object Main extends App {
 
   private val DAYS_OF_WEEK = List("M", "T", "W", "Th", "F", "S", "Su")
-  type DowTimePeople = (String, TimeRange, List[String])
+  type Dow = String
+  type Name = String
+  type DowTimePeople = (Dow, TimeRange, List[Name])
+  type DowTimePeopleV2 = (Dow, TimeRanges, List[Name])
   private def main(): Unit = {
 
     // Read file
@@ -19,18 +22,24 @@ object Main extends App {
     // Parse CSV
     val parsed = SimpleCSVParser.parse(lines, omitFirstLine = false)
 
-    val availableTimeAndPeople = findAvailableTimeAndPeople(parsed)
+    val availableTimeAndPeople = findAvailableTimeAndPeopleV2(parsed)
     val daysWithMaxPeople = getOnlyDaysWithMaxPeople(availableTimeAndPeople)
     val sortedByPeopleCount = availableTimeAndPeople.sortBy(_._3.length).reverse
     // Outputs
 
-    printDowTimePeopleList(availableTimeAndPeople, Some("Available time and people each day:"))
-    printDowTimePeopleList(daysWithMaxPeople, Some("Only days with max people:"))
-    printDowTimePeopleList(sortedByPeopleCount, Some("Sorted by people count:"))
+    printDowTimePeopleList(
+      availableTimeAndPeople,
+      Some("Available time and people each day:")
+    )
+//    printDowTimePeopleList(daysWithMaxPeople, Some("Only days with max people:"))
+//    printDowTimePeopleList(sortedByPeopleCount, Some("Sorted by people count:"))
 
   }
 
-  private def printDowTimePeopleList( dowTimePeopleList: List[DowTimePeople], message: Option[String] = None): Unit = {
+  private def printDowTimePeopleList(
+      dowTimePeopleList: List[DowTimePeople],
+      message: Option[String] = None
+  ): Unit = {
     println(message.getOrElse(""))
     for (dowTimePeople <- dowTimePeopleList) {
       println(dowTimePeople)
@@ -58,6 +67,66 @@ object Main extends App {
 
       (dow, availableTime, availablePeople)
     })
+  }
+
+  private def findAvailableTimeAndPeopleV2(
+      parsedCSV: List[List[String]]
+  ): List[DowTimePeople] = {
+    // for each day:
+    //  turn timeData into TimeEvents
+    //  sort TimeEvents by time
+    //  iterate TimeEvents to find TimeRanges:
+    //    if avail = max number of people
+    //      record start
+    //    if avail no longer at max
+    //      record end
+    //      create a TimeRange, add it to TimeRanges
+    //
+
+    DAYS_OF_WEEK.map((dow: String) => {
+      val timeData = parsedCSVToTimeDataList(parsedCSV, dow)
+      val timeEvents = timeData
+        .map(td => new TimeEvent(td.timeStart, 1))
+        .concat(timeData.map(td => new TimeEvent(td.timeEnd, -1)))
+      val sorted = timeEvents.sortBy(_.time)
+
+      println(sorted)
+
+      val timeRanges = findTimeRanges(sorted, timeData.groupBy(_.name).size)
+
+      println(timeRanges)
+      println()
+
+      val availableTime = findAvailableTime(timeData, new TimeRange())
+      val availablePeople = findAvailablePeople(availableTime, timeData)
+
+      (dow, availableTime, availablePeople)
+    })
+  }
+  @tailrec
+  private def findTimeRanges(
+      tes: List[TimeEvent], // should be sorted by time
+      max: Int,
+      count: Int = 0,
+      tr: TimeRange = new TimeRange(-1,-1),
+      trs: TimeRanges = new TimeRanges(List[TimeRange]())
+  ): TimeRanges = {
+    val te = tes.head
+    if(tes.nonEmpty){
+//      println(tes + " " + max + " " + count + " " + tr + " " + trs)
+      val newCount = count + te.event
+      if(newCount == max){
+        findTimeRanges(tes.tail, max, newCount, new TimeRange(te.time, tr.end), trs)
+      }else{
+        if(new TimeRange(tr.start, te.time).isValidTime){
+          findTimeRanges(tes.tail, max, newCount, new TimeRange(-1,-1), trs.add(new TimeRange(tr.start, te.time)))
+        }else{
+          findTimeRanges(tes.tail, max, newCount, tr, trs)
+        }
+      }
+    }else{
+      trs.get
+    }
   }
 
   private def parsedCSVToTimeDataList(
